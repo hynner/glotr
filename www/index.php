faba027b8690491b31db6e48fd0e89b6e582bf00
hynner <hynner@seznam.cz>
2013-05-17 14:13:58 +0200

    check config files. close #1

--- www/index.php
+++ www/index.php
@@ -1,17 +1,7 @@
 <?php
-
 // uncomment this line if you must temporarily take down your site for maintenance
 // require '.maintenance.php';
+require "paths.php";
 
-// absolute filesystem path to this web root
-define('WWW_DIR', __DIR__);
-
-// absolute filesystem path to the application root
-define('APP_DIR',  '../app');
-
-// absolute filesystem path to the libraries
-define('LIBS_DIR',  '../libs');
-// you can use this value to have multiple configurations
-define('CONF_DIRNAME', 'config');
 // load bootstrap file
 require APP_DIR . '/bootstrap.php';
---
+++ www/paths.php
@@ -0,0 +1,11 @@
+<?php
+// absolute filesystem path to this web root
+define('WWW_DIR', __DIR__);
+
+// absolute filesystem path to the application root
+define('APP_DIR',  '../app');
+
+// absolute filesystem path to the libraries
+define('LIBS_DIR',  '../libs');
+// you can use this value to have multiple configurations
+define('CONF_DIRNAME', 'config');
