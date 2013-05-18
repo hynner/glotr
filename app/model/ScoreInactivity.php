f09047b1d69b5ec8ec0d219b0bf24bd52076e25c
hynner <hynner@seznam.cz>
2013-05-18 11:34:21 +0200

    score inactivity search form + planets

--- app/model/ScoreInactivity.php
+++ app/model/ScoreInactivity.php
@@ -11,35 +11,63 @@ class ScoreInactivity extends Table
 	{
 		$this->getTable()->where("NOT id_player", $ids)->delete();
 	}
-	public function search($paginator = NULL)
+	public function search($paginator = NULL, $filter = array())
 	{
 		$playersT = $this->container->players->getTableName();
-		$inactT = $this->getTableName();;
+		$inactT = $this->getTableName();
+		$univT = $this->container->universe->getTableName();
 		$where = "where $inactT.duration > 0 and $playersT.status = ''";
-		$order = "order by $inactT.duration DESC";
+		$group = "group by score_inactivity.id_player";
+		$params = array();
+		if(!empty($filter))
+		{
+			if(isset($filter["galaxy"]) && $filter["galaxy"] !== "")
+			{
+				$where .= " and $univT.galaxy = ?";
+				$params[] = $filter["galaxy"];
+			}
+			if(isset($filter["system_start"]) && $filter["system_start"] !== "")
+			{
+				$where .= " and $univT.system >= ?";
+				$params[] = $filter["system_start"];
+			}
+			if(isset($filter["system_end"]) && $filter["system_end"] !== "")
+			{
+				$where .= " and $univT.system <= ?";
+				$params[] = $filter["system_end"];
+			}
+			if(isset($filter["score_0_position_s"]) && $filter["score_0_position_s"] !== "")
+			{
+				$where .= " and $playersT.score_0_position >= ?";
+				$params[] = $filter["score_0_position_s"];
+			}
+			if(isset($filter["score_0_position_e"]) && $filter["score_0_position_e"] !== "")
+			{
+				$where .= " and $playersT.score_0_position <= ?";
+				$params[] = $filter["score_0_position_e"];
+			}
+		}
+		$order = "order by $inactT.duration DESC, playername ASC";
 		$limit = "";
 		if($paginator !== NULL)
 		{
-			$what = "count($inactT.id_player) as c";
-			$query = "select $what from $inactT join $playersT on $inactT.id_player = $playersT.id_player_ogame $where $order $limit;";
-			$count = $this->connection->query($query)->fetch();
-			if($count !== FALSE)
+			$what = "count($inactT.id_player)";
+			$query = "select $what from $inactT join $playersT on $inactT.id_player = $playersT.id_player_ogame join $univT on $inactT.id_player = $univT.id_player $where $group $order $limit;";
+			$count = $this->invokeQuery($query, $params);
+			if(!empty($count) )
 			{
-				$paginator->setItemCount($count->c);
+				$count = count($count);
+				$paginator->setItemCount($count);
 				$paginator->setItemsPerPage(50);
 				$limit = "limit ".$paginator->getOffset().",".$paginator->getLength();
 			}
 
 		}
-		$what = "$inactT.*, $playersT.playername as playername";
-		$query = "select $what from $inactT join $playersT on $inactT.id_player = $playersT.id_player_ogame $where $order $limit;";
-		$res = $this->connection->query($query);
-		$ret = array();
-		while($r = $res->fetch())
-		{
-			$ret[$r->id_player] = $r;
-		}
-		return $ret;
+
+
+		$what = "$inactT.*, $playersT.playername as playername,$playersT.score_0_position as pos, GROUP_CONCAT(galaxy,':',system,':',position SEPARATOR ' ') as planets";
+		$query = "select $what from $inactT join $playersT on $inactT.id_player = $playersT.id_player_ogame join $univT on $inactT.id_player = $univT.id_player $where $group $order $limit;";
+		return $this->invokeQuery($query, $params);
 	}
 
 
